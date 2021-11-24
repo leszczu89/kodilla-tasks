57ad08220b25b52df721d1fc456d7f511e77a4ae
Paulina Leszczynska <paulinarudz@wp.pl>
2021-11-24 17:43:19 +0100

    cross origin changed

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin("*")
+@CrossOrigin(origins = "https://stormy-depths-24142.herokuapp.com")
 @RestController
 @RequestMapping(value = "/v1/trello")
 @RequiredArgsConstructor
